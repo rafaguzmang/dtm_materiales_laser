ff51dbec9daec95556dfd16ad7e2161b54855a83
raguzman <55261233+rafaguzmang@users.noreply.github.com>
2023-12-29 08:49:12 -0600

    Se agregan dos opciones:  - Ver los trabajos pendientes  - Ver los travajos realizados
     Este proceso resta el material del modulo "Materiales"

--- __manifest__.py
+++ __manifest__.py
@@ -1,8 +1,14 @@
 {
     "name":"Materiales Laser",
+
+    'version': '1.0',
+    'author': "Rafael Guzmán",
     "description": "Conteo de materiales de salida",
     "data":[        
         'security/ir.model.access.csv',
-        'views/dtm_materiales_laser_views.xml'
+        'views/dtm_materiales_laser_views.xml',
+        'views/dtm_laser_realizados_view.xml',
+        'views/menu_item.xml'
     ]
 }
+
