a3ef8156c324f80ea25b553eb81ef8c9e6cc1839
raguzman <55261233+rafaguzmang@users.noreply.github.com>
2024-01-31 14:48:45 -0600

    Se quita en el boton de descontar que reste de la tabla de materiales de almacén

--- models/dtm_materiales_laser.py
+++ models/dtm_materiales_laser.py
@@ -3,39 +3,84 @@ from odoo import api,models,fields
 
 class MaterialesLasser(models.Model):
     _name = "dtm.materiales.laser"
+    _description = "Lleva el listado de los materiales a cortar en la laser"
 
     material = fields.Char(string="MATERIAL", readonly=True)
     calibre = fields.Char(string="CALIBRE", readonly=True)
-    largo = fields.Char(string="LARGO", default="0", readonly=True)
-    ancho = fields.Char(string="ANCHO", default="0", readonly=True)
-    cantidad = fields.Integer(string="CANTIDAD", readonly=True)
-
+    largo = fields.Char(string="LARGO", readonly=True)
+    ancho = fields.Char(string="ANCHO", readonly=True)
+    sheets = fields.Char(string="SHEETS", readonly=True)
+    drawingname  = fields.Char(string="DRAWINGNAME", readonly=True)
+    material_id = fields.Integer()
 
     #--------------------  Llena la tabla con las ordenes de servicio ----------------
 
-    def acction_less(self):
-        cantidad = self.cantidad - 1
-        if cantidad <= 0:
-            cantidad = "0"
+    def acction_less(self):#----------------------- Borra los procesos ya realizados en la coratadora laser y en el modulo de diseño pasandolos a otra tabla donde se llevará el registro
+        # get_cantidad = self.env["dtm.materiales"].browse(self.material_id)
+        # cantidad = get_cantidad.cantidad - int(self.sheets)
+
+        material = self.material
+        calibre = self.calibre
+        largo = self.largo
+        ancho = self.ancho
+        sheets = self.sheets
+        drawingname = self.drawingname
+        material_id = str(self.material_id)
+
+        # Descuenta los materiales ya utilizados del stock
+        # self.env.cr.execute("UPDATE dtm_materiales SET cantidad = "+str(cantidad)+" WHERE id = "+str(self.material_id))
+        # Pasa la orden de pendientes a realizados en el modulo de dtm_materiales_laser
+        self.env.cr.execute("INSERT INTO dtm_laser_realizados (material, calibre, largo, ancho, sheets, drawingname, material_id)" +
+                            "VALUES ('"+material+"', '"+calibre+"', '"+largo+"', '"+ancho+"', '"+sheets+"', '"+drawingname+"', "+material_id+");")
+        # Retira las ordenes ya cortadas de Cortadora Laser
+        self.env.cr.execute("DELETE FROM dtm_materiales_laser WHERE material_id = "+str(material_id))
+
+        get_material = self.env['dtm.materiales'].search([("id","=",material_id)])
+
+        list_material =  '{} - CALIBRE: {} LARGO:  {} ANCHO: {} '.format(get_material.material_id.nombre,
+                                                                         get_material.calibre_id.calibre,
+                                                                         get_material.largo_id.largo,
+                                                                         get_material.ancho_id.ancho)
+
+        # print(list_material)
+        # Pasa la orden de pendientes a realizados en el modulo de dtm_diseno de Diseño a Realizados
+        self.env.cr.execute("INSERT INTO dtm_diseno_realizados (drawingname, sheets, material_id)" +
+                            "VALUES ('"+drawingname+"', '"+sheets+"', '"+list_material+"');")
+        # Retira las ordenes ya cortadas de Diseño
+        self.env.cr.execute("DELETE FROM dtm_diseno WHERE drawingname = '"+drawingname+"'")
 
 
-        self.env.cr.execute("UPDATE dtm_materiales SET cantidad = "+str(cantidad)+" WHERE id = "+str(self.id))
-        self.cantidad = cantidad
 
-
-    def get_view(self, view_id=None, view_type='form', **options):
+    def get_view(self, view_id=None, view_type='form', **options):#-----------------Llena la tabla dtm_materiales_laser con los nesteos programados por diseño----------
         res = super(MaterialesLasser,self).get_view(view_id, view_type)
 
-        get_info = self.env['dtm.materiales'].search([])
-        self.env.cr.execute("DELETE FROM dtm_materiales_laser")
+        get_info = self.env['dtm.diseno'].search([])
+        self.env.cr.execute("DELETE FROM dtm_materiales_laser ")
+        cont = 0;
 
         for result in get_info:
-            material = result.material_id.nombre
-            calibre = result.calibre_id.calibre
-            largo = result.largo_id.largo
-            ancho = result.ancho_id.ancho 
-            cantidad = str(result.cantidad)  
-            id = str(result.id)          
-            self.env.cr.execute("INSERT INTO dtm_materiales_laser (id, material, calibre, largo, ancho, cantidad)"+
-                         " VALUES ("+id+", '"+material +"','"+ calibre +"','"+ largo +"','"+ ancho +"',"+cantidad+")")  
+            print(result.material_id.id)
+            material = str(result.material_id.material_id.nombre)
+            calibre = str(result.material_id.calibre_id.calibre)
+            largo = str(result.material_id.largo_id.largo)
+            ancho = str(result.material_id.ancho_id.ancho)
+            sheets = str(result.sheets)
+            drawingname = str(result.drawingname)
+            material_id = str(result.material_id.id)
+            cont += 1
+            self.env.cr.execute("INSERT INTO dtm_materiales_laser ( id, material, calibre, largo, ancho, sheets, drawingname, material_id) " +
+                                "VALUES("+str(cont)+",'"+material+"', '"+calibre+"','"+largo+"','"+ancho+"','"+sheets+"','"+drawingname+"', "+ material_id +")")
+
         return res
+
+class Realizados(models.Model): #--------------Muestra los trabajos ya realizados---------------------
+    _name = "dtm.laser.realizados"
+    _description = "Lleva el listado de todo el material cortado en la Laser"
+
+    material = fields.Char(string="MATERIAL", readonly=True)
+    calibre = fields.Char(string="CALIBRE", readonly=True)
+    largo = fields.Char(string="LARGO", readonly=True)
+    ancho = fields.Char(string="ANCHO", readonly=True)
+    sheets = fields.Char(string="SHEETS", readonly=True)
+    drawingname  = fields.Char(string="DRAWINGNAME", readonly=True)
+    material_id = fields.Integer()
